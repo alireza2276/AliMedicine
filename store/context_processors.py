eaf9dba0679650374592c02064e613b995cb7984
alireza <alirezaii802@gmail.com>
2024-01-28 21:52:44 +0330

    add context processors

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,7 +1,10 @@
 from .models import Category
+from django.shortcuts import get_object_or_404
 
 
-def show_category(request):
-    category = Category.objects.order_by('title').prefetch_related('products').prefetch_related('subs').select_related('parent')
+def show_category(request, parent_id=None):
+
+    category = Category.objects.prefetch_related('subs__subs').select_related('parent__parent')
 
     return { 'category': category}
+
